fb4fb463e4ab7774208dba6a5c52341392b8fdce
Andrew Condie <andrew.condie@mre.medical.canon>
2023-11-15 16:32:34 +0000

    Add invalid pairings, primarily for partners.

--- SecretSanta/Program.cs
+++ SecretSanta/Program.cs
@@ -1,27 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecretSanta
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var Names = new string[] {
-                "Name1",
-				"Name2"
-            };
+        readonly struct Partners {
+            public readonly string First;
+            public readonly string Second;
 
+            public Partners(string first, string second) {
+                First = first;
+                Second = second;
+            }
+
+            public bool IsInvalid(string name_a, string name_b) {
+                if (First == name_a) {
+                    return Second == name_b;
+                }
+                if (Second == name_a) {
+                    return First == name_b;
+                }
+                return false;
+            }
+        }
+
+        static readonly string[] Names = new[] {
+            "Name1",
+            "Name2",
+            "Name3"
+        };
+        static readonly Partners[] InvalidPairs = new Partners[] {
+            new Partners("Name1", "Name2")
+        };
+
+
+        static void Main() {
             var shuffled_indices = new int[Names.Length];
             for (int i = 0; i < shuffled_indices.Length; ++i) {
                 shuffled_indices[i] = i;
             }
 
             var rand = new Random();
-            while (!IsShuffled(shuffled_indices)) {
+            while (!IsShuffledAndValid(shuffled_indices)) {
                 int index = rand.Next(shuffled_indices.Length);
                 int to_swap = rand.Next(shuffled_indices.Length);
 
@@ -35,9 +56,15 @@ namespace SecretSanta
             }
         }
 
-        static bool IsShuffled(int[] indices) {
+        static bool IsShuffledAndValid(int[] indices) {
             for (int i = 0; i < indices.Length; ++i) {
                 if (indices[i] == i) {
+                    return false;
+                }
+
+                var name_a = Names[i];
+                var name_b = Names[indices[i]];
+                if (InvalidPairs.Any(invalid_pair => invalid_pair.IsInvalid(name_a, name_b))) {
                     return false;
                 }
             }
